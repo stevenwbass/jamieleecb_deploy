da03f990fa3433ecacb5d12d6a8a83b741c9eb68
Steven Bass <stevenwbass@gmail.com>
2024-11-27 15:32:54 -0500

    map port 80 to port 80

--- deploy_webapi.ps1
+++ deploy_webapi.ps1
@@ -2,4 +2,4 @@
 
 podman pull docker.io/ursteveb/dotnetstoreapi:v8-latest
 
-sudo podman run -d --name storeapi -p 80:8080 docker.io/ursteveb/dotnetstoreapi:v8-latest
+sudo podman run -d --name storeapi -p 80:80 docker.io/ursteveb/dotnetstoreapi:v8-latest
